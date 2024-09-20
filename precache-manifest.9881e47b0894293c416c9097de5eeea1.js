1956f73a285f842ae0731cbcd42abbceab2d51e9
erick.villalva <erick_289_777@@hotmail.com>
2024-09-20 15:19:45 +0300

    Updates

---
+++ precache-manifest.9881e47b0894293c416c9097de5eeea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de724259afac2cd36f7c2ea1ebe03ac5",
+    "url": "/capac-urcu-frontend/index.html"
+  },
+  {
+    "revision": "77c0e84a6ccbf4e255ed",
+    "url": "/capac-urcu-frontend/static/css/main.ab6ce702.chunk.css"
+  },
+  {
+    "revision": "009300a17d20b0a4f99d",
+    "url": "/capac-urcu-frontend/static/js/2.6af2b424.chunk.js"
+  },
+  {
+    "revision": "8c3214d3d6e31cb577d7646cc2e8506b",
+    "url": "/capac-urcu-frontend/static/js/2.6af2b424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c0e84a6ccbf4e255ed",
+    "url": "/capac-urcu-frontend/static/js/main.bc0899f5.chunk.js"
+  },
+  {
+    "revision": "b956c129fca47943751c",
+    "url": "/capac-urcu-frontend/static/js/runtime-main.c55914f2.js"
+  },
+  {
+    "revision": "2aa836fd56c2b6ab01417e70943a2527",
+    "url": "/capac-urcu-frontend/static/media/logo.2aa836fd.png"
+  }
+]);
